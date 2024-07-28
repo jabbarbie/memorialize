d0d27465d2d43c2998650817014947480bd47eb1
Fauzi Jabbar <krotopal@gmail.com>
2024-07-28 14:07:51 +0700

    git command

---
+++ app/Console/Commands/DeployGit.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\Process\Process;
+
+class DeployGit extends Command
+{
+    protected $signature = 'deploy:git';
+    protected $description = 'Pull the latest changes from git and copy public files to /home/plkmyid/public_html';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $this->info('Starting deployment...');
+
+        // Jalankan git pull
+        $gitPull = new Process(['git', 'pull']);
+        $gitPull->run(function ($type, $buffer) {
+            echo $buffer;
+        });
+
+        if (!$gitPull->isSuccessful()) {
+            $this->error('Failed to pull latest changes from git.');
+            Log::error('Failed to pull latest changes from git: ' . $gitPull->getErrorOutput());
+            return;
+        }
+
+        $this->info('Successfully pulled latest changes from git.');
+
+        // Copy files dari public ke /home/plkmyid/public_html
+        $source = public_path();
+        $destination = '/home/plkmyid/public_html';
+
+        if (!File::exists($destination)) {
+            $this->error('Destination directory does not exist.');
+            Log::error('Destination directory does not exist: ' . $destination);
+            return;
+        }
+
+        File::copyDirectory($source, $destination);
+
+        $this->info('Successfully copied public files to ' . $destination);
+        Log::info('Successfully copied public files to ' . $destination);
+    }
+}
