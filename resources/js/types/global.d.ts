da05e2ddca9c70caf8ef10e1d7330ec6ed84350e
Fauzi Jabbar <krotopal@gmail.com>
2024-07-28 13:25:45 +0700

    kembalikan ziggy

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,35 +1,10 @@
-declare module 'ziggy-js' {
-    interface ZiggyRoute {
-        uri: string;
-        methods: string[];
-        domain?: null | string;
+import { AxiosInstance } from 'axios';
+import { route as ziggyRoute } from 'ziggy-js';
+
+declare global {
+    interface Window {
+        axios: AxiosInstance;
     }
 
-    interface Ziggy {
-        namedRoutes: Record<string, ZiggyRoute>;
-        baseUrl: string;
-        baseProtocol: string;
-        baseDomain: string;
-        basePort?: null | number;
-        defaultParameters: [];
-    }
-
-    export function route(
-        name: string,
-        params?: Record<string, any>,
-        absolute?: boolean,
-        customZiggy?: Ziggy
-    ): string;
-    export function current(name?: string): boolean;
+    var route: typeof ziggyRoute;
 }
-
-// import { AxiosInstance } from 'axios';
-// import { route as ziggyRoute } from 'ziggy-js';
-
-// declare global {
-//     interface Window {
-//         axios: AxiosInstance;
-//     }
-
-//     var route: typeof ziggyRoute;
-// }
